cb6856e4d6778431053c1e09ab64f374eccd90a8
Anthony Thorne <athorne@thecode.co>
2024-10-22 17:45:13 +1000

    Update the block editor registration controller

--- src/php/Controller/BlockEditorRegistrationController.php
+++ src/php/Controller/BlockEditorRegistrationController.php
@@ -14,7 +14,7 @@ namespace Enqueues\Controller;
 use Enqueues\Base\Main\Controller;
 use function Enqueues\asset_find_file_path;
 use function Enqueues\is_local;
-use function get_translation_domain;
+use function Enqueues\get_translation_domain;
 
 /**
  * Controller responsible for Block Editor related functionality.
@@ -78,111 +78,83 @@ class BlockEditorRegistrationController extends Controller {
 	public function block_categories( $categories, $post ) {
 
 		$directory = get_template_directory();
-		$icon      = null;
 
-		$block_editor_categories = apply_filters ( 'enqueues_block_editor_categories', [] );
+		$block_editor_categories = apply_filters( 'enqueues_block_editor_categories', [] );
 
 		if ( ! $block_editor_categories ) {
 			return $categories;
 		}
 
 		foreach ( $block_editor_categories as $category ) {
-
 			// Skip if the category is not properly defined.
 			if ( ! isset( $category['slug'] ) || ! isset( $category['title'] ) ) {
 				continue;
 			}
 
 			if ( isset( $category['icon'] ) ) {
-
-				$icon = $category['icon'];
-
-				$svg_file = "{$directory}{$icon}";
-
-				if ( file_exists( $svg_file ) ) {
-
-					// Load the contents of the SVG file
-					$svg_content = file_get_contents( $svg_file ); // phpcs:ignore
-
-					if ( $svg_content ) {
-
-						// Base64 encode the contents
-						$encoded_data = base64_encode( $svg_content ); // phpcs:ignore
-
-						// Embed the base64 encoded data into an image tag
-						$icon = '<img src="data:image/svg+xml;base64,' . $encoded_data . '" alt="Custom Icon" width="20" height="20">';
-					}
-				}
-
-				$category['title'] = __( $category['title'], get_translation_domain() ); // phpcs:ignore
-				$category['icon']  = $icon;
+				$icon_path        = $directory . $category['icon'];
+				$category['icon'] = get_encoded_svg_icon( $icon_path );
 			}
 
-			$categories = array_merge( $categories, [ $category ] );
+			$category['title'] = __( $category['title'], get_translation_domain() );
+			$categories        = array_merge( $categories, [ $category ] );
 		}
 
 		return $categories;
 	}
 
 	/**
-	 * Enqueue assets for a given type and context.
+	 * Enqueue assets for blocks
 	 *
-	 * @param string $type    The type of asset to enqueue (plugins or extensions).
-	 * @param string $context The context in which to enqueue the asset (frontend or admin).
+	 * @param string $type          The type of asset to enqueue (blocks, plugins, or extensions).
+	 * @param string $context       The context in which to enqueue the asset (frontend, editor, or view).
+	 * @param bool   $register_only Whether to only register the asset or also enqueue it.
+	 *
+	 * @return void
 	 */
 	private function enqueue_assets( $type, $context, $register_only = true ) {
-
 		$directory             = get_template_directory();
 		$directory_uri         = get_template_directory_uri();
-		$block_editor_dist_dir = $directory . "/dist/block-editor/{$type}";
-		$enqueue_asset_dirs    = array_filter( glob( $block_editor_dist_dir . '/*' ), 'is_dir' );
+		$block_editor_dist_dir = "{$directory}/dist/block-editor/{$type}";
+		$enqueue_asset_dirs    = array_filter( glob( "{$block_editor_dist_dir}/*" ), 'is_dir' );
 
 		foreach ( $enqueue_asset_dirs as $enqueue_asset_dir ) {
-
 			$filename = basename( $enqueue_asset_dir );
-			$name     = 'caretochange/' . $filename;
+			$name     = apply_filters( "enqueues_block_editor_name_{$type}", 'enqueues/' . $filename );
 
-			// Enqueue the CSS bundle for the asset.
+			// Enqueue the CSS bundle.
 			$css_filetype = $this->get_filename_from_context( $context, 'css' );
 			$css_path     = asset_find_file_path( "dist/block-editor/{$type}/{$filename}", $css_filetype, 'css', $directory );
 
 			if ( $css_path ) {
-				wp_register_style( "{$name}-{$css_filetype}", rtrim( $directory_uri, '/' ) . $css_path, 'all', filemtime( $directory . $css_path ) );
+				wp_register_style( "{$name}-{$css_filetype}", rtrim( $directory_uri, '/' ) . $css_path, [], filemtime( "{$directory}{$css_path}" ) );
 
-				// Enqueue the CSS bundle for the asset if its not set to register only. Register only is used for localization of vars.
 				if ( ! $register_only ) {
-					wp_enqueue_script( "{$name}-{$css_filetype}" );
+					wp_enqueue_style( "{$name}-{$css_filetype}" );
 				}
 			}
 
-			// Enqueue the JS bundle for the asset.
+			// Enqueue the JS bundle.
 			$js_filetype = $this->get_filename_from_context( $context, 'js' );
 			$js_path     = asset_find_file_path( "dist/block-editor/{$type}/{$filename}", $js_filetype, 'js', $directory );
 
 			if ( $js_path ) {
-				$enqueue_asset_path = $directory . '/' . ltrim( str_replace( '.js', '.asset.php', $js_path ), '/' );
+				$enqueue_asset_path = "{$directory}/" . ltrim( str_replace( '.js', '.asset.php', $js_path ), '/' );
 
 				if ( file_exists( $enqueue_asset_path ) ) {
 					$assets = include $enqueue_asset_path;
-
 					wp_register_script( "{$name}-{$js_filetype}", rtrim( $directory_uri, '/' ) . $js_path, $assets['dependencies'], $assets['version'], true );
 
-					// Enqueue the JS bundle for the asset if its not set to register only. Register only is used for localization of vars.
 					if ( ! $register_only ) {
 						wp_enqueue_script( "{$name}-{$js_filetype}" );
 					}
 
-					$localized_data     = apply_filters( "enqueues_block_editor_localized_data_{$type}_{$name}", [], $filename );
-					$localized_var_name = apply_filters( "enqueues_block_editor_localized_data_var_name_{$type}_{$name}", 'customBlockEditor' . ucfirst( $type ) . 'Config' );
+					$localized_data     = apply_filters( "enqueues_block_editor_localized_data_{$type}_{$filename}", [] );
+					$localized_var_name = apply_filters( "enqueues_block_editor_localized_data_var_name_{$type}_{$filename}", 'customBlockEditor' . ucfirst( $type ) . 'Config' );
 
-					// Skip if the localized data is empty.
-					if ( ! $localized_data ) {
-						continue;
+					if ( $localized_data ) {
+						wp_localize_script( "{$name}-{$js_filetype}", $localized_var_name, $localized_data );
 					}
-
-					wp_localize_script( "{$name}-{$js_filetype}", $localized_var_name, $localized_data );
-				} elseif ( is_local() ) {
-					wp_die( sprintf( 'Run npm build for the Block Editor asset files, the %s file is missing.', $enqueue_asset_path ), E_USER_ERROR ); // phpcs:ignore
 				}
 			}
 		}
